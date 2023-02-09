4246e8881719dec099e1a1d21608510f1e648d31
PIPablo <105118051+BustosPablo@users.noreply.github.com>
2023-02-09 19:51:32 -0300

    CORS enabled.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
+import cors from "cors";
 import rutas from "./routers/productos_routers.js";
+
 
 const app = express();
 
 /* Settings */
 app.set('json spaces', 2);
 app.use(express.json());
+app.use(cors());
 
 /* Rutas */
 app.use(rutas);
