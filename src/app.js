a8b22356a7ddde0e69494ab99244eb96fb5fdb00
PIPablo <105118051+BustosPablo@users.noreply.github.com>
2023-02-09 20:23:54 -0300

    Test CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import {cors} form "cors";
 import rutas from "./routers/productos_routers.js";
 
 
@@ -8,9 +7,6 @@ const app = express();
 /* Settings */
 app.set('json spaces', 2);
 app.use(express.json());
-app.use(cors({
-  origin: "http://127.0.0.1:5500/"
-}))
 
 /* Rutas */
 app.use(rutas);
