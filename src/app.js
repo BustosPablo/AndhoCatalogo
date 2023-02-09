67e3eda99830ea8918eabf495231e4e53eab6232
PIPablo <105118051+BustosPablo@users.noreply.github.com>
2023-02-09 20:34:56 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import rutas from "./routers/productos_routers.js";
+const cors = require('cors');
 
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 /* Settings */
 app.set('json spaces', 2);
 app.use(express.json());
+app.use(cors());
 
 /* Rutas */
 app.use(rutas);
