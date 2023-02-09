e7dcad3d7f6c3fd3969c305e9c61f7bdf4c6e2b1
PIPablo <105118051+BustosPablo@users.noreply.github.com>
2023-02-09 20:37:42 -0300

    Repair Code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
 import express from "express";
 import rutas from "./routers/productos_routers.js";
-const cors = require('cors');
-
 
 const app = express();
 
 /* Settings */
 app.set('json spaces', 2);
 app.use(express.json());
-app.use(cors());
 
 /* Rutas */
 app.use(rutas);
