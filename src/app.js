5e227973fc0247d26b99d5045d152f76bef0ee87
PIPablo <105118051+BustosPablo@users.noreply.github.com>
2023-02-09 20:06:01 -0300

    DELETE CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import {cors} from "cors";
 import rutas from "./routers/productos_routers.js";
 
 
@@ -8,7 +7,6 @@ const app = express();
 /* Settings */
 app.set('json spaces', 2);
 app.use(express.json());
-app.use(cors());
 
 /* Rutas */
 app.use(rutas);
