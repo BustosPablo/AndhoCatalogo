91798d0ffbb0c12b42ded49182d4a390d278946f
PIPablo <105118051+BustosPablo@users.noreply.github.com>
2023-02-09 20:51:57 -0300

    Enabale CORS

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import rutas from "./routers/productos_routers.js";
 
 const app = express();
 
 /* Settings */
+app.use(cors());
 app.set('json spaces', 2);
 app.use(express.json());
 
