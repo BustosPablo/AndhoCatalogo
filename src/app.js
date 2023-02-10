ebe95b7e513479a904e20caa32300e501586de16
PIPablo <105118051+BustosPablo@users.noreply.github.com>
2023-02-09 21:07:35 -0300

    Headers config

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from "express";
-import cors from "cors";
 import rutas from "./routers/productos_routers.js";
 
 const app = express();
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 /* Settings */
-app.use(cors());
 app.set('json spaces', 2);
 app.use(express.json());
 
