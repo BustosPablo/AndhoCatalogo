2b7e8484badd91c9dea22f65551a5280e8dcb1ca
Andho-Catalogo <127954660+Andho-Catalogo@users.noreply.github.com>
2023-03-22 22:36:48 -0300

    Update validation method
    Add Bcrypt and Express-sessions

--- src/controllers/usuario_controller.js
+++ src/controllers/usuario_controller.js
@@ -1,13 +1,22 @@
 const pool = require("../db.js");
+const bcrypt = require('bcryptjs');
 
 /* Traer usuario */
 const getUsuario = async (req, res) =>{
+    const {nombre, contrasena} = req.body;
     try {
-        const result = await pool.query('SELECT * FROM usuario');
-        res.json(result[0]);
+        const [result] = await pool.query('SELECT * FROM usuario WHERE nombre = ?', [nombre]);
+        if(result.length>0 && result[0].nombre === nombre && bcrypt.compareSync(contrasena, result[0].contrasena)){
+            req.session.loggedin= true;
+            req.session.usuario = nombre;
+            res.status(200).json({message: "Login OK"});
+        }else{
+            res.status(404).json({message: "Verificar usuario o contraseña"});
+        }
     } catch (error) {
         return res.status(500).json({ message: "Usuario no encontrado" });
     };  
 };
+
 
 module.exports = getUsuario;
